acc5466f9df91096a519bca080715d8e97b0809f
sushantpaudel <paudelsushant482@gmail.com>
2023-06-21 15:14:24 +0545

    complete nepal's flag

--- unit-6/NepalFlag.java
+++ unit-6/NepalFlag.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 public class NepalFlag extends JFrame {
   NepalFlag() {
     this.setTitle("Flag of Nepal");
-    this.setSize(1000, 1000);
+    this.setSize(500, 500);
     this.setVisible(true);
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   }
@@ -16,6 +16,30 @@ public class NepalFlag extends JFrame {
     // g.fillRect(30, 30, 10, 600);
     // g.fillPolygon(x1Points, y1Points, 5);
     // g.fillOval(100, 100, 100, 60);
+
+    // BLUE TRIANGLE
+    g.setColor(Color.BLUE);
+    int x1Points[] = { 100, 300, 150, 350, 100 };
+    int y1Points[] = { 100, 200, 200, 350, 350 };
+    g.fillPolygon(x1Points, y1Points, 5);
+
+    // RED TRIANGLE
+    g.setColor(Color.RED);
+    int x2Points[] = { 110, 270, 130, 330, 110 };
+    int y2Points[] = { 115, 190, 190, 340, 340 };
+    g.fillPolygon(x2Points, y2Points, 5);
+
+    // SUN
+    g.setColor(Color.WHITE);
+    g.fillOval(130, 250, 80, 80);
+
+    // MOON
+    g.setColor(Color.WHITE);
+    g.fillOval(120, 140, 40, 40);
+    g.setColor(Color.RED);
+    g.fillOval(120, 130, 40, 40);
+    g.setColor(Color.WHITE);
+    g.fillOval(130, 160, 20, 20);
   }
 
   public static void main(String[] args) {
